51183a030780b40ba586eb9c3d4e1824eb64afc1
burakaltiparmak <mburakaltiparmak@gmail.com>
2024-03-20 02:02:16 +0300

    Öğrenci numarası girildi. Palindrom sayıyı bulma görevi yapıldı.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,23 @@
 package org.example;
 
 public class Main {
+
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        int number = 12321;
+        isPalindrome(number);
+    }
+    public static boolean isPalindrome(int number) {
+        String numString =String.valueOf(number);
+        char[] digits = numString.toCharArray();
+
+        for (int i = 0, j=digits.length-1; i<j ; i++,j--){
+            if(digits[i] != digits[j]) {
+                System.out.println("Sayı palindrom değil!" + number);
+                return  false;
+            }
+        }
+        System.out.println("Sayı palindrom!" + number);
+        return true;
     }
 }
